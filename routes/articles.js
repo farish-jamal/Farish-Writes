79b89ff3c080609b86985c95d6ebf798204f6afc
farish-jamal <farishjamal98@gmail.com>
2022-10-31 13:18:32 +0530

    Updaate

--- routes/articles.js
+++ routes/articles.js
@@ -4,13 +4,18 @@ const Article = require("../models/article");
 const router = express.Router();
 
 router.get("/new", (req, res)=>{
-    res.render("articles/new")
+    res.render("articles/new", {article: article})
 })
 
 router.get("/:id", async(req,res)=>{
     let article = await Article.findById(req.params.id);
     if(article == null) res.redirect("/");
     res.render("articles/post", {article: article});
+})
+
+router.get("/edit/:id", async(req,res)=>{
+    let article = await Article.findById(req.params.id);
+    res.render("articles/edit", {article: article});
 })
 
 router.post("/", async(req, res)=>{
@@ -29,4 +34,25 @@ router.post("/", async(req, res)=>{
     }
 })
 
+router.put("/:id", async(req, res)=>{
+    let article = await Article.findById(req.params.id);
+    console.log(article);
+    article.imgUrl = req.body.imgUrl
+    article.title = req.body.title
+    article.description = req.body.description
+    article.fullArticle = req.body.fullArticle
+    try {
+        article = await article.save();
+        // console.log(article);
+        res.redirect(`/articles/${article.id}`);
+    } catch (error) {
+        res.render("articles/edit", {article: article});
+    }
+    // console.log(article);
+})
+
+router.delete("/:id", async(req, res)=>{
+    let article = await Article.findByIdAndDelete(req.params.id);
+    res.redirect("/");
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const mongoose = require("mongoose");
 const Article = require("./models/article");
 const articleRouter = require("./routes/articles");
+var methodOverride = require('method-override')
 const app = express()
+const port = 3000;
 
-mongoose.connect("mongodb+srv://farishjamal:farishjamal1234@cluster0.2otv6pq.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb://localhost:27017/myBlog");
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'))
 
 app.get('/', async(req, res) => {
     let article = await Article.find().sort({createdAt: "desc"});
@@ -17,4 +20,6 @@ app.get('/', async(req, res) => {
 
 app.use("/articles", articleRouter);
 
-app.listen(process.env.PORT || 3000);
+app.listen(port, ()=> {
+    console.log("Connection at" + port);
+});
